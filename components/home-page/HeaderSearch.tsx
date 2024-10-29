a7d46d7e25efb34fa5a1c173d43c8756ee144d98
salartalaA <salarmhmdn@gmail.com>
2024-10-29 10:35:57 +0330

    add a mobile search bar to project

---
+++ components/home-page/HeaderSearch.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import searchIcon from '@/public/assets/images/home-page/search 2.png'
+
+const HeaderSearch = () => {
+  return (
+    <div className="md:hidden flex flex-row justify-center p-3 rounded-md gap-[10px] min-w-[200px] min-h-[40px] bg-primary-lighter items-center">
+      <Image src={searchIcon} alt="" />
+      <input className="outline-none border-none bg-transparent" placeholder="جست و جو در رویداد‌ ..." />
+    </div>
+  );
+};
+export default HeaderSearch;
