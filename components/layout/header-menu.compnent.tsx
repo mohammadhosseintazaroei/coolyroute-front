dcd47109f6ab605ec78569aa178125cdd5315a93
salartalaA <salarmhmdn@gmail.com>
2024-10-31 19:38:49 +0330

    better header menu and responsive

--- components/layout/header-menu.compnent.tsx
+++ components/layout/header-menu.compnent.tsx
@@ -48,11 +48,11 @@ const HeaderMenu = (props: Props) => {
       hamburgerMenuItem:
         "bg-light  block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ",
       mainMenuWrapper:
-        "h-full flex flex-col md:flex-row gap-10 md:gap-14 top-0 right-0 max-md:absolute max-md:bg-primary-light max-md:w-full max-md:flex-col p-4 transition-[right] duration-300 z-[110]",
+        "sm:h-full h-[390px] flex flex-col md:flex-row gap-10 md:gap-14 top-0 right-0 max-md:absolute max-md:bg-primary-light max-md:w-full max-md:flex-col p-4 transition-[right] duration-300 z-[110]",
       menuItem:
         "text-lg my-auto sm:text-base lg:text-lg transition-[border] duration-500 hover:border-b-2-[red]",
       userAccountButton:
-        "transition duration-200 rounded-full size-12 max-md:size-10 flex justify-center items-center relative  hover:shadow-button bg-transparent border-2 border-primary hover:bg-primary-light",
+        "transition duration-200 rounded-full size-12 max-md:size-10 flex justify-center items-center relative  hover:shadow-button bg-transparent border-2 border-primary hover:bg-primary-light z-50",
       userAccountTab:
         "transition-[background] duration-200 w-fit absolute top-full left-0 rounded-xl p-4 mt-3 flex flex-col items-start gap-2 transition duration-200",
       accountTabItem: "flex gap-3 whitespace-pre items-center max-md:text-sm",
@@ -105,7 +105,10 @@ const HeaderMenu = (props: Props) => {
         <TransitionLink href="/events" className={`${styles.menuItem()}`}>
           رویداد ها
         </TransitionLink>
-        <TransitionLink href="/" className={`${styles.menuItem()}`}>
+        <TransitionLink
+          href="/"
+          className={`${styles.menuItem()} hidden md:block`}
+        >
           <Image src={logo} alt="logo" className="w-[90px] h-[87.98px]" />
         </TransitionLink>
         <TransitionLink href="/about" className={`${styles.menuItem()}`}>
