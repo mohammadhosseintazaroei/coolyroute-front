e9192f28ef26996eaa516beb724265b073f6f26c
salartalaA <salarmhmdn@gmail.com>
2024-11-02 22:12:46 +0330

    solved the header problem

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -1,16 +1,24 @@
+'use client'
+
 import HeaderMenu from "./header-menu.compnent";
 import HeaderSearch from "./header-search.component";
+import { usePathname } from "next/navigation";
+
 interface Props {}
 
-// eslint-disable-next-line react-hooks/rules-of-hooks
 const Navbar = (props: Props) => {
+  const pathname = usePathname();
+  const isHomePage = pathname === "/";
+
   return (
-    <>
-      <nav className="fixed container top-0 left-0 right-0 py-6 px-4 flex justify-between items-center mx-auto z-50">
-        <HeaderSearch />
-        <HeaderMenu />
-      </nav>
-    </>
+    <nav
+      className={`container top-0 left-0 right-0 py-6 px-4 flex justify-between items-center mx-auto z-50 ${
+        isHomePage ? "fixed" : "sticky"
+      }`}
+    >
+      <HeaderSearch />
+      <HeaderMenu />
+    </nav>
   );
 };
 
