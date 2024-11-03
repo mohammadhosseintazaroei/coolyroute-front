b2434736763e6879cfe8469671fdd6ae8483f3c9
salartalaA <salarmhmdn@gmail.com>
2024-11-03 23:09:12 +0330

    text color addded

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import HeaderMenu from "./header-menu.compnent";
 import HeaderSearch from "./header-search.component";
@@ -12,7 +12,7 @@ const Navbar = (props: Props) => {
 
   return (
     <nav
-      className={`container top-0 left-0 right-0 py-6 px-4 flex justify-between items-center mx-auto z-50 ${
+      className={`container top-0 left-0 right-0 py-6 px-4 flex justify-between items-center mx-auto z-50 text-foreground ${
         isHomePage ? "fixed" : "sticky"
       }`}
     >
